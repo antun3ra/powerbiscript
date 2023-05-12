15a1c9c056c18ce1f4aac015744c576e1be6be52
Fabio Antunes <fabioal@gmail.com>
2023-05-12 17:22:40 -0300

    updating

--- import_script.py
+++ import_script.py
@@ -1,2 +1,7 @@
 import pandas as pd
-import sqllite3 
+import sqlite3
+from file_path import path # file with file path to .db file 
+
+conn = sqlite3.connect(path)
+
+df = pd.read_sql_query('SELECT * FROM invoice_line;', conn)

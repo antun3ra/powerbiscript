53f9e7e73a11ab588631e3dd095b69e1053cae9c
Fabio Antunes <fabioal@gmail.com>
2023-05-16 19:42:31 -0300

    updating

--- import_script.py
+++ import_script.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import sqlite3
-from file_path import path # file with file path to .db file 
+from file_path import path  # file with file path to .db file
 
 conn = sqlite3.connect(path)
 
-df = pd.read_sql_query('SELECT * FROM invoice_line;', conn)
+df = pd.read_sql_query('SELECT * FROM genre;', conn)
+
+df.to_csv('genre.csv')
